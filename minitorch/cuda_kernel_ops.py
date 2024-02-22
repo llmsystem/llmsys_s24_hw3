68aab40a1d907cd05146cd466800ec0f8e8918f1
Jeremy Lee <mengchil@cs.cmu.edu>
2024-02-22 09:22:19 -0500

    Update cuda_kernel_ops.py

--- minitorch/cuda_kernel_ops.py
+++ minitorch/cuda_kernel_ops.py
@@ -24,7 +24,6 @@ import torch
 
 # Load the shared library
 lib = ctypes.CDLL("minitorch/cuda_kernels/combine.so")
-lib_mm = ctypes.CDLL("minitorch/cuda_kernels/batched_matmul.so")
 lib_softmax = ctypes.CDLL("minitorch/cuda_kernels/softmax_kernel.so")
 lib_layernorm = ctypes.CDLL("minitorch/cuda_kernels/layernorm_kernel.so")
 datatype = np.float32
